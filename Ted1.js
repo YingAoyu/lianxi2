f3f9fe1ad8ec12ff5022d53d9160db43cb50b76a
YingAoyu <103395208+YingAoyu@users.noreply.github.com>
2022-04-19 11:24:17 +0800

    Add files via upload

---
+++ Ted1.js
@@ -0,0 +1,6 @@
+function Ted1(){
+    var x = document.getElementById("myText").value; 
+    var y = document.getElementById("Text1").value;
+    var z= Number(x) + Number(y);
+    document.getElementById("demo").innerHTML = z ;
+    }
