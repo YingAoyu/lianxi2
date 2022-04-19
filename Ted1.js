ab41cc4c10daa8c1b117fd14154b718db7b6175f
YingAoyu <103395208+YingAoyu@users.noreply.github.com>
2022-04-19 11:21:35 +0800

    Delete Ted1.js

--- Ted1.js
+++
@@ -1,6 +0,0 @@
-function Ted1(){
-    var x = document.getElementById("myText").value; 
-    var y = document.getElementById("Text1").value;
-    var z= Number(x) + Number(y);
-    document.getElementById("demo").innerHTML = z ;
-    }
